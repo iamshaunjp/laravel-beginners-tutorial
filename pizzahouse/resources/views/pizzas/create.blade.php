eceb893623bb9b423651ac2418a888743589b2bf
Shaun <shaun@thenetninja.co.uk>
2020-01-09 15:54:51 +0000

    lesson-18

--- pizzahouse/resources/views/pizzas/create.blade.php
+++ pizzahouse/resources/views/pizzas/create.blade.php
@@ -1,14 +1,26 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Create a new pizza
-        </div>
-
-       
-
-    </div>
+<div class="wrapper create-pizza">
+  <h1>Create a New Pizza</h1>
+  <form action="/pizzas" method="POST">
+    <label for="name">Your name:</label>
+    <input type="text" name="name" id="name">
+    <label for="type">Choose type of pizza:</label>
+    <select name="type" id="type">
+      <option value="margarita">Margarita</option>
+      <option value="hawaiian">Hawaiian</option>
+      <option value="veg supreme">Veg Supreme</option>
+      <option value="volcano">Volcano</option>
+    </select>
+    <label for="base">Choose crust:</label>
+    <select name="base" id="base">
+      <option value="thick">Thick</option>
+      <option value="thin & crispy">Thin & Crispy</option>
+      <option value="cheese crust">Cheese Crust</option>
+      <option value="garlic crust">Garlic Crust</option>
+    </select>
+    <input type="submit" value="Order Pizza">
+  </form>
 </div>
 @endsection
