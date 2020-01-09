40f15644d46ed97f8070dd7f29a0ad30d896865c
Shaun <shaun@thenetninja.co.uk>
2020-01-09 14:16:44 +0000

    lesson-15

--- pizzahouse/app/Http/Controllers/PizzaController.php
+++ pizzahouse/app/Http/Controllers/PizzaController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Pizza;
 
 class PizzaController extends Controller
 {
 
   public function index() {
-    // get data from a database
-    $pizzas = [
-      ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-      ['type' => 'volcano', 'base' => 'garlic crust'],
-      ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
+
+    // $pizzas = Pizza::all();  
+    // $pizzas = Pizza::orderBy('name', 'desc')->get();
+    // $pizzas = Pizza::where('type', 'hawaiian')->get();
+    $pizzas = Pizza::latest()->get();      
 
     return view('pizzas', [
       'pizzas' => $pizzas,
--- pizzahouse/resources/views/pizzas.blade.php
+++ pizzahouse/resources/views/pizzas.blade.php
@@ -9,13 +9,7 @@
 
         @foreach($pizzas as $pizza)
           <div>
-            {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
-            @if($loop->first)
-              <span> - first in the loop</span>
-            @endif
-            @if($loop->last)
-              <span> - last in the loop</span>
-            @endif
+            {{ $pizza['name'] }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
           </div>
         @endforeach
 
