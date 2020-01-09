de4d68592b35002ab1f2f4961d2adcca4d2a1470
Shaun <shaun@thenetninja.co.uk>
2020-01-09 11:46:28 +0000

    lesson-7

--- pizzahouse/resources/views/pizzas.blade.php
+++ pizzahouse/resources/views/pizzas.blade.php
@@ -1,96 +1,32 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.layout')
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title m-b-md">
-                    Pizza List
-                </div>
-                
-                <!-- @for($i = 0; $i < 5; $i++)
-                  <p>the value of i is {{ $i }}</p>
-                @endfor -->
-
-                <!-- @for($i = 0; $i < count($pizzas); $i++)
-                  <p>{{ $pizzas[$i]['type'] }}</p>
-                @endfor -->
-
-                @foreach($pizzas as $pizza)
-                  <div>
-                    {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
-                    @if($loop->first)
-                      <span> - first in the loop</span>
-                    @endif
-                    @if($loop->last)
-                      <span> - last in the loop</span>
-                    @endif
-                  </div>
-                @endforeach
-
-            </div>
+@section('content')
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        <div class="title m-b-md">
+            Pizza List
         </div>
-    </body>
-</html>
+        
+        <!-- @for($i = 0; $i < 5; $i++)
+          <p>the value of i is {{ $i }}</p>
+        @endfor -->
+
+        <!-- @for($i = 0; $i < count($pizzas); $i++)
+          <p>{{ $pizzas[$i]['type'] }}</p>
+        @endfor -->
+
+        @foreach($pizzas as $pizza)
+          <div>
+            {{ $loop->index }} - {{ $pizza['type'] }} - {{ $pizza['base'] }}
+            @if($loop->first)
+              <span> - first in the loop</span>
+            @endif
+            @if($loop->last)
+              <span> - last in the loop</span>
+            @endif
+          </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
