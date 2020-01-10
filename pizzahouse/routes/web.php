c81f4b7c680e96ffb0475421b695ca17eda5a7a0
Shaun <shaun@thenetninja.co.uk>
2020-01-10 10:09:32 +0000

    lesson-24

--- pizzahouse/routes/web.php
+++ pizzahouse/routes/web.php
@@ -21,3 +21,7 @@ Route::get('/pizzas/create', 'PizzaController@create');
 Route::post('/pizzas', 'PizzaController@store');
 Route::get('/pizzas/{id}', 'PizzaController@show');
 Route::delete('/pizzas/{id}', 'pizzaController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
