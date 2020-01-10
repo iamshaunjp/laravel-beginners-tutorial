4494073009080cbb488e61772ee8708a840cff19
Shaun <shaun@thenetninja.co.uk>
2020-01-10 09:36:56 +0000

    lesson-23

--- pizzahouse/resources/views/pizzas/index.blade.php
+++ pizzahouse/resources/views/pizzas/index.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
-        </div>
-
-        @foreach($pizzas as $pizza)
-          <div>
-            <a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a>
-          </div>
-        @endforeach
-
+<div class="wrapper pizza-index">
+  <h1>Pizza Orders</h1>
+  @foreach($pizzas as $pizza)
+    <div class="pizza-item">
+      <img src="/img/pizza.png" alt="pizza icon">
+      <h4><a href="/pizzas/{{ $pizza->id }}">{{ $pizza->name }}</a></h4>
     </div>
+  @endforeach
+
 </div>
+
 @endsection
